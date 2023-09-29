330359d309dec4bb802bc5fdabc77092ae0dbad8
QuietCraftsman <145918763+QuietCraftsman@users.noreply.github.com>
2023-09-29 21:42:52 +0300

    Basic DEBUG macro. Standard headers added.

--- include/warlock.h
+++ include/warlock.h
@@ -1,10 +1,18 @@
 #ifndef _WARLOCK_H
 #define _WARLOCK_H
 
+#include <stdbool.h>
+#include <stdint.h>
+
 #include "static-texts.h"
 #include "unicode-symbols.h"
 
 // TODO: Move all settings to config.mk or so.
+#define DEBUG
+
+#ifdef DEBUG
+#include <assert.h>
+#endif
 
 #define USE_UNICODE
 
